6dd01993af61093fa3ad1c7d5571e9339e70b861
jiangwenyang <wenyang.jay@gmail.com>
2025-12-17 14:28:04 +0800

    fix: 修复

--- src/stores/project.ts
+++ src/stores/project.ts
@@ -1,36 +1,46 @@
 // stores/project.ts
-import { defineStore } from 'pinia';
-import { http } from '../http';
+import { defineStore } from "pinia";
+import { http } from "../http";
 
-export const useProjectStore = defineStore('project', {
+export const useProjectStore = defineStore("project", {
   state: () => ({
-    projects: JSON.parse(localStorage.getItem('projects') || '[]') || [],
+    projects: JSON.parse(localStorage.getItem("projects") || "[]") || [],
     currentProject: {
-      _id: {}
+      _id: {},
     },
   }),
   actions: {
     updateProjects(projects) {
       this.projects = projects;
-      localStorage.setItem('projects', JSON.stringify(projects));
+      localStorage.setItem("projects", JSON.stringify(projects));
     },
     updateCurrentProject(project) {
       this.currentProject = project;
     },
     updateCurrentProjectById(projectId) {
-      this.currentProject = this.projects.find(x => x._id.$oid === projectId);
+      this.currentProject = this.projects.find((x) => x._id.$oid === projectId);
     },
     async refreshProject(projectId) {
       const result = await http.get(`/api/project_info/${projectId}`);
-      this.currentProject = result.project_data;
+
+      if (result.project_data) {
+        const index = this.projects.findIndex(
+          (item) => item._id.$oid === projectId
+        );
+        if (index !== -1) {
+          this.projects.splice(index, 1, result.project_data);
+          this.currentProject = this.projects[index];
+        }
+      }
     },
     async refreshAllProjects() {
-      const result = await http.get('/api/display_user_projects', { })
-        .then(response => {
-            this.updateProjects(response.data);
-            return response.data;
+      const result = await http
+        .get("/api/display_user_projects", {})
+        .then((response) => {
+          this.updateProjects(response.data);
+          return response.data;
         });
       return result;
-    }
+    },
   },
 });
