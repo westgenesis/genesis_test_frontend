d35779559f9b512b303f277ae9d04e349439292e
jiangwenyang <wenyang.jay@gmail.com>
2026-01-12 17:02:10 +0800

    feat: 删除

--- src/components/Requirement/RequirementManage.vue
+++ src/components/Requirement/RequirementManage.vue
@@ -56,8 +56,13 @@
             >
               解析
             </el-button>
-            <!-- <el-button type="primary"
-                  size="small" danger @click="doDelete(record)">删除</el-button> -->
+            <el-button
+              type="primary"
+              size="small"
+              danger
+              @click="doDelete(record)"
+              >删除</el-button
+            >
           </div>
         </template>
       </template>
